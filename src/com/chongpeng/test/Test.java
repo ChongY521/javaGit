750bd424b2e983fa237af2f67dd997d9c17cb770
chongpeng <1062568521@qq.com>
2020-11-09 21:10:58 +0800

    111

--- src/com/chongpeng/test/Test.java
+++ src/com/chongpeng/test/Test.java
@@ -2,7 +2,7 @@ package com.chongpeng.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("giteee");
+        System.out.println("gitccc");
         System.out.println("git722");
     }
 }

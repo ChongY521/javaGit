20f1b544e816615b66b97eefd8a628c1bc46320d
chongpeng <1062568521@qq.com>
2020-11-09 20:47:05 +0800

    99

--- src/com/chongpeng/test/Test.java
+++ src/com/chongpeng/test/Test.java
@@ -2,14 +2,6 @@ package com.chongpeng.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
-        System.out.println("git666");
-        System.out.println("git888");
         System.out.println("git777");
         System.out.println("git777");
     }
